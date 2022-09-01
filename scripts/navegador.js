a759ebb9a9b2de2a5171d97d3c01a1cb15d58f24
Daniel Quijano <hector.daniel.quijano@gmail.com>
2022-09-01 18:13:16 -0300

    version1

---
+++ scripts/navegador.js
@@ -0,0 +1,6 @@
+let menuDropdown = document.querySelector(".menuDropdown");
+let navegadorDropdown = document.querySelector(".navegadorDropdown");
+
+menuDropdown.onclick = function(){
+    navegadorDropdown.classList.toggle("active")
+}
